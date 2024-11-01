23a1fdfb312f63cc92c23a6c5ee41d1c679ccf27
sammy456pok922 <ian@sh.co.ke>
2024-11-01 12:09:10 +0300

    Update tic_tac_toe.js

--- tic_tac_toe.js
+++ tic_tac_toe.js
@@ -1,5 +1,8 @@
 
 let occupied_cells=[];
+let yourId;
+
+let lastConnection;
  let playerACells=[];
  let playerBCells=[];
  let gamesPlayed=0;
@@ -124,8 +127,7 @@ timeParagraph.innerText=secondsTaken+"s";
 
 function resetGame(){
 
-    document.getElementById("playerBW").style.display="none";
-    document.getElementById("playerAW").style.display="none";
+    
     let gameCells=document.getElementsByClassName("cell")
 
     for(let index=0;index<gameCells.length; index++){
@@ -150,8 +152,7 @@ function resetGame(){
 
 }
 
-let modalHeading=document.getElementById("winning_header")
-function cellClicked(clickedDiv){
+function play( clickedDiv){
     if (nextPlayer===1){
         add_circle_to_div(clickedDiv)
         playerACells.push(clickedDiv.id)
@@ -197,6 +198,17 @@ function cellClicked(clickedDiv){
         }
         
     }
+}
+
+let modalHeading=document.getElementById("winning_header")
+function cellClicked(clickedDiv){
+
+    if (lastConnection){
+        client.publish(lastConnection+"/play", clickedDiv.id)
+    }
+
+play(clickedDiv);
+    
     
  document.getElementById("playerAScores").innerText="W"+playerAwins+"L"+playerAloss
  document.getElementById("playerBScores").innerText="W"+playerBwins+"L"+playerBloss
@@ -208,9 +220,125 @@ function cellClicked(clickedDiv){
 
 
 
+function makeid(length) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      counter += 1;
+    }
+    return result;
+}
+
+yourId=makeid(6);
+
+let yourIdDisplay=document.getElementById("yourId")
+yourIdDisplay.innerText=yourId
 
 
 
+const url = 'wss://broker.emqx.io:8084/mqtt'
+
+// Create an MQTT client instance
+const options = {
+  // Clean session
+  clean: true,
+  connectTimeout: 4000,
+  // Authentication
+  clientId: yourId,
+  username: 'emqx_test',
+  password: 'emqx_test',
+}
+const client  = mqtt.connect(url, options)
+client.on('connect', function () {
+  console.log('Connected')
+  // Subscribe to a topic
+  client.subscribe(yourId+"/#", function (err) {
+    if (!err) {
+      // Publish a message to a topic
+      client.publish(yourId+"/test", 'Connection test')
+    }
+  })
+})
+
+
+// Receive messages
+client.on('message', function (topic, message) {
+  // message is Buffer
+  console.log(message.toString())
+  console.log(topic)
+
+  let action=topic.split('/')[1]
+  console.log(action)
+
+  if (action==="connect"){
+
+if (!lastConnection){
+    client.publish(message.toString()+"/connect", yourId)
+    lastConnection=message.toString();
+    let partneridDisplay=document.getElementById("partnerId")
+partneridDisplay.innerText=message.toString()
+
+console.log("Connecting to "+message.toString())
+}
+
+  }
+
+  if(action=="play"){
+    let div_id = message.toString()
+    console.log(div_id)
+    let clickedDiv=document.getElementById(div_id)
+    play(clickedDiv);
+  }
+
+  if (action=="message"){
+
+    let messageDisplay=document.getElementById("messageDisplay")
+    let newP=document.createElement("p")
+    newP.innerText=message
+    newP.classList.add("partner")
+    messageDisplay.appendChild(newP);
+
+  }
+
+//   client.end()
+})
+
+function connectWithPartner(){
+let connectionPartner=document.getElementById("connectionId").value
+let topic=connectionPartner+"/connect"
+
+client.publish(topic, yourId)
+console.log(topic)
+
+
+
+}
+
+function sendMessage(){
+let message= document.getElementById("messageInput").value
+if (message==""){
+    alert("Provide a valid message")
+}
+else{
+    if (!lastConnection){
+        alert("You are not connected to anyone")
+    }
+    else{
+        let messageDisplay=document.getElementById("messageDisplay")
+        let newP=document.createElement("p")
+        newP.innerText=message
+        newP.classList.add("me")
+        messageDisplay.appendChild(newP);
+        client.publish(lastConnection+"/message", message)
+    }
+}
+
+
+
+}
 
 
 
